4a5a73d22dc752311c76813a1d3d58c310dff869
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2025-11-17 16:53:09 +0100

    hide at 20

--- les-compteurs/src/Components/count.jsx
+++ les-compteurs/src/Components/count.jsx
@@ -5,12 +5,14 @@ const Compteur = () => {
     const colorButton = useMemo(() => {
         return count < 10 ? 'button-color-green' : 'button-color-red';
     }, [count]);
-
+    const hideButton = useMemo(() => {
+        return count >= 20 ? 'hideButton' : '';
+    }, [count]);
 
     return (
         <>
             <div className='compteur-container'>
-                <button className={colorButton} onClick={() => setCount(count => count + 2)}>
+                <button className={`${colorButton} ${hideButton}`} onClick={() => setCount(count => count + 2)}>
 
                     count is {count}
                 </button>
