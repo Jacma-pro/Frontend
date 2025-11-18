2b3d54b985ebd5cefeb93c2f73f36696a21ef529
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2025-11-18 11:09:57 +0100

    size

--- les-compteurs/src/App.jsx
+++ les-compteurs/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <>
+      <h1>Try to reach 20</h1>
       <Compteur />
       <Compteur />
     </>
--- les-compteurs/src/Components/count.jsx
+++ les-compteurs/src/Components/count.jsx
@@ -2,30 +2,41 @@ import React, { useState, useMemo } from 'react';
 
 const Compteur = () => {
     const [count, setCount] = useState(0);
-    const colorButton = useMemo(() => {
+    const handleColorButton = useMemo(() => {
         return count < 10 ? 'button-color-green' : 'button-color-red';
     }, [count]);
-    const handlehideButton = useMemo(() => {
+    const handleHideButton = useMemo(() => {
         return count >= 20 ? 'hideButton' : '';
+    }, [count]);
+    const handleButtonSize = useMemo(() => {
+        return {
+            width: `${150 - count * 10}px`,
+            height: `${50 - count * 4}px`,
+            fontSize: `${20 - count * 2}px`
+        };
     }, [count]);
 
     const [isHovered, setIsHovered] = useState(false);
-    const handlebuttonHoveredClass = useMemo(() => {
+    const handleButtonHoveredClass = useMemo(() => {
         return isHovered ? 'button-hover' : '';
     }, [isHovered]);
 
     return (    
         <>
             <div className='compteur-container'>
-                <button className={`${colorButton} ${hideButton} ${buttonHoveredClass}`} onClick={() => setCount(count => count + 2)} 
+                <button className={`${handleColorButton} ${handleHideButton} ${handleButtonHoveredClass}`} 
+                    onClick={() => setCount(count => count + 2)} 
+                    style={handleButtonSize}
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
-                >
-                    count is {count}
+                > <br />
                 </button>
                 <button onClick={() => setCount(0)}>
                     Reset
                 </button>
+                <p>
+                    Count is {count}
+                </p>
             </div>
         </>
     );
