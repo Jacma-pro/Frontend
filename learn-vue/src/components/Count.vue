b0ea5878cdd1a59a843245e685205ccf40a54eb5
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2025-12-07 17:57:23 +0100

    count

---
+++ learn-vue/src/components/Count.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <div class="counter">
+    <h2>Compteur</h2>
+    <p>Compteur : {{ count }}</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
